1adb3027a25e50996e7a18a4cf654e071d04f9ae
ly <15901839273@163.com>
2018-09-04 15:02:48 +0800

    测试上传文件

--- api/inc/upload.php
+++ api/inc/upload.php
@@ -9,10 +9,10 @@
 
 
 
-namespace Qiniu;
+namespace hivebanks\inc;
 
 use Qiniu\Auth;
-
+require_once   '../plugin/Qiniu/functions.php';
 
 ini_set("display_errors", "On");
 error_reporting(E_ALL | E_STRICT);
