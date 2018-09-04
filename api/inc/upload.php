987d93fb80ab1ce4748fc2babd43f678aa7c303d
ly <15901839273@163.com>
2018-09-04 15:56:11 +0800

    测试上传文件

--- api/inc/upload.php
+++ api/inc/upload.php
@@ -18,7 +18,7 @@ $object = "img";
 $content = "Hi, OSS.";
 try {
     print_r(66);
-    $ossClient = new OSS\OssClient($accessKeyId, $accessKeySecret, $endpoint);
+    $ossClient = new \OSS\OssClient($accessKeyId, $accessKeySecret, $endpoint);
     print_r($ossClient);
     $ossClient->putObject($bucket, $object, $content);
     print_r(22);
