2349c7def2b5ffe70b4ca19927ae89a083a36d06
ly <15901839273@163.com>
2018-09-04 18:11:05 +0800

    测试上传文件

--- api/upload/upload_file.php
+++ api/upload/upload_file.php
@@ -50,7 +50,7 @@ if ($_FILES["file"]["size"] > $sizeLimit)
 // 定义新文件名
 $ext = explode('.', basename($filename));
 
-$target = "/public/img" . DIRECTORY_SEPARATOR . md5(uniqid()) . "." . array_pop($ext);
+$target = "../public/img" . DIRECTORY_SEPARATOR . md5(uniqid()) . "." . array_pop($ext);
 print_r($target);
 
 
