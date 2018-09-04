bdfc72bef00046c1f2c40869a89a05445e28edb4
ly <15901839273@163.com>
2018-09-04 15:55:53 +0800

    测试上传文件

--- api/inc/upload.php
+++ api/inc/upload.php
@@ -6,22 +6,19 @@ require_once 'autoload.php';
 
 ini_set("display_errors", "On");
 error_reporting(E_ALL | E_STRICT);
-print_r(333);
 use OSS\OssClient;
 use OSS\Core\OssException;
-print_r(444);
 $accessKeyId = "LTAIuTfkvjnNg54j";
 $accessKeySecret = "OTETap8a971xgfYdNCawWuHTkbR5dj";
 // Endpoint以杭州为例，其它Region请按实际情况填写。
 $endpoint = "oss-cn-beijing.aliyuncs.com";
 // 存储空间名称
 $bucket = "hivebanks";
-print_r(555);
 $object = "img";
 $content = "Hi, OSS.";
 try {
     print_r(66);
-    $ossClient = new OssClient($accessKeyId, $accessKeySecret, $endpoint);
+    $ossClient = new OSS\OssClient($accessKeyId, $accessKeySecret, $endpoint);
     print_r($ossClient);
     $ossClient->putObject($bucket, $object, $content);
     print_r(22);
