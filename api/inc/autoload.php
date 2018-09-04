002c8cb16f97326554919ae1ef6bc4e85ffbce8f
ly <15901839273@163.com>
2018-09-04 16:43:43 +0800

    测试上传文件

--- api/inc/autoload.php
+++ api/inc/autoload.php
@@ -4,7 +4,7 @@ function classLoader($class)
 {
     $path = str_replace('\\', DIRECTORY_SEPARATOR, $class);
     $file = __DIR__  . DIRECTORY_SEPARATOR . $path . '.php';
-    print_r(DIRECTORY_SEPARATOR);
+    print_r($file);
     if (file_exists($file)) {
         require_once $file;
     }
