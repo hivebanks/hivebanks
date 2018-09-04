63c5364fbde1dc02855dfc29566b2553110e577e
ly <15901839273@163.com>
2018-09-04 14:41:02 +0800

    1

--- api/inc/upload.php
+++ api/inc/upload.php
@@ -22,24 +22,24 @@ $secretKey = 'mYzXiGuWtfLVsqyyowW0rUjh3IIczb2GzoTmFelT';
 $auth = new Auth($accessKey, $secretKey);
 
 
-
-/* 上传 */
-//////////////////////////////////////////////////////////////////////////
-
-// 要上传的空间
-$bucket = 'richie';
-
-//自定义上传回复的凭证 返回的数据
-$returnBody = '{"key":"$(key)","hash":"$(etag)","fsize":$(fsize),"bucket":"$(bucket)","name":"$(fname)"}';
-$policy = array(
-    'returnBody' => $returnBody,
-
-);
-////token过期时间
-$expires = 3600;
+//
+///* 上传 */
+////////////////////////////////////////////////////////////////////////////
+//
+//// 要上传的空间
+//$bucket = 'richie';
+//
+////自定义上传回复的凭证 返回的数据
+//$returnBody = '{"key":"$(key)","hash":"$(etag)","fsize":$(fsize),"bucket":"$(bucket)","name":"$(fname)"}';
+//$policy = array(
+//    'returnBody' => $returnBody,
+//
+//);
+//////token过期时间
+//$expires = 3600;
 //
 //// 生成上传 Token
-$token = $auth->uploadToken($bucket, null, $expires, $policy, true);
+//$token = $auth->uploadToken($bucket, null, $expires, $policy, true);
 //
 //// 要上传文件的本地路径
 //$filePath = './img-08.jpg'; // 上传到七牛后保存的文件名，可拼接
