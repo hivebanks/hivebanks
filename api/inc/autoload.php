ce25170d7cdda975c54332700130f650883d68de
ly <15901839273@163.com>
2018-09-04 16:40:45 +0800

    测试上传文件

--- api/inc/autoload.php
+++ api/inc/autoload.php
@@ -4,6 +4,7 @@ function classLoader($class)
 {
     $path = str_replace('\\', DIRECTORY_SEPARATOR, $class);
     $file = __DIR__ . DIRECTORY_SEPARATOR .'src'. DIRECTORY_SEPARATOR . $path . '.php';
+    print_r($file);
     if (file_exists($file)) {
         require_once $file;
     }
