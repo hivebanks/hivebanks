0f3479d795638bc8fce900482effccef3e5d64aa
ly <15901839273@163.com>
2018-09-04 15:52:57 +0800

    测试上传文件

--- api/inc/upload.php
+++ api/inc/upload.php
@@ -1,11 +1,8 @@
 <?php
 
-if (is_file(__DIR__ . '/autoload.php')) {
-    require_once __DIR__ . '/autoload.php';
-}
-if (is_file(__DIR__ . '/../vendor/autoload.php')) {
-    require_once __DIR__ . '/../vendor/autoload.php';
-}
+require_once 'autoload.php';
+
+
 
 ini_set("display_errors", "On");
 error_reporting(E_ALL | E_STRICT);
