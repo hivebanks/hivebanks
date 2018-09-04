e4fa438948c547c85add22b1e5a756305fd12fab
ly <15901839273@163.com>
2018-09-04 15:47:24 +0800

    测试上传文件

--- api/inc/upload.php
+++ api/inc/upload.php
@@ -6,15 +6,14 @@ print_r(333);
 use OSS\OssClient;
 use OSS\Core\OssException;
 print_r(444);
-// 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
-$accessKeyId = "<yourAccessKeyId>";
-$accessKeySecret = "<yourAccessKeySecret>";
+$accessKeyId = "LTAIuTfkvjnNg54j";
+$accessKeySecret = "OTETap8a971xgfYdNCawWuHTkbR5dj";
 // Endpoint以杭州为例，其它Region请按实际情况填写。
-$endpoint = "http://oss-cn-hangzhou.aliyuncs.com";
+$endpoint = "oss-cn-beijing.aliyuncs.com";
 // 存储空间名称
-$bucket = "<yourBucketName>";
+$bucket = "hivebanks";
 print_r(555);
-$object = " <yourObjectName>";
+$object = "img";
 $content = "Hi, OSS.";
 try {
     $ossClient = new OssClient($accessKeyId, $accessKeySecret, $endpoint);
