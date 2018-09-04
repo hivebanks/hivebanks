e97d9ea7f14a73e6ec7c5e3ddd629e691ba59868
ly <15901839273@163.com>
2018-09-04 16:48:52 +0800

    测试上传文件

--- api/plugin/OSS/autoload.php
+++ api/plugin/OSS/autoload.php
@@ -3,7 +3,7 @@
 function classLoader($class)
 {
     $path = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-    print_r($path."\n");
+    print_r(dirname(__FILE__)."\n");
     $file = __DIR__  . DIRECTORY_SEPARATOR . $path . '.php';
     print_r($file);
     if (file_exists($file)) {
