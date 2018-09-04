fa404b27b1f35497ef0dde1eb4c049110c388b44
ly <15901839273@163.com>
2018-09-04 15:41:41 +0800

    测试上传文件

--- api/inc/upload.php
+++ api/inc/upload.php
@@ -2,9 +2,10 @@
 
 require_once  'autoload.php';
 
-
+print_r(333);
 use OSS\OssClient;
 use OSS\Core\OssException;
+print_r(444);
 // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
 $accessKeyId = "<yourAccessKeyId>";
 $accessKeySecret = "<yourAccessKeySecret>";
