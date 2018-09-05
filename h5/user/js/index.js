0ab4ef45a9d7297308bf4bd2ac22f6e081a864a8
Edwin <edwin@Edwin-MacBook-Pro.local>
2018-09-05 13:13:51 +0800

    edwin

--- h5/user/js/index.js
+++ h5/user/js/index.js
@@ -1,4 +1,39 @@
-$(function(){
+$(function () {
+    var login_us_token = GetQueryString('user_token');
+    var login_ba_token = GetQueryString('ba_token');
+    var login_ca_token = GetQueryString('ca_token');
+
+    function GetIndexCookie(name) {
+        var arr = document.cookie.match(new RegExp("(^| )" + name + "=([^;]*)(;|$)"));
+        if (arr != null) return unescape(arr[2]);
+    }
+
+    var user_token = GetIndexCookie('user_token');
+    var ba_token = GetIndexCookie('ba_token');
+    var ca_token = GetIndexCookie('ca_token');
+    if (user_token) {
+        $('.noLogin').remove();
+        $('.alreadyLogin, .create_btn').removeClass('none');
+    }
+    if (ba_token) {
+        $('.noLogin').remove();
+        $('.alreadyLogin').removeClass('none');
+    }
+    if (ca_token) {
+        $('.noLogin').remove();
+        $('.alreadyLogin').removeClass('none');
+    }
+    $('.toAccountBtn').click(function () {
+        if (login_us_token || user_token) {
+            window.location.href = 'user/account.html';
+        }
+        if (login_ba_token || ba_token) {
+            window.location.href = 'user/BaAccount.html';
+        }
+        if (login_ca_token || ca_token) {
+            window.location.href = 'user/CaAccount.html';
+        }
+    });
     var smokyBG = $('#smoky-bg').waterpipe({
         gradientStart: '#51ff00',
         gradientEnd: '#001eff',
--- h5/user/js/main.js
+++ h5/user/js/main.js
@@ -4,7 +4,23 @@ $(function () {
         $(".jt").toggleClass("active");
         $(".msg-content").slideToggle();
     });
-
+    //back index
+    function getCurrentPath() {
+        //获取当前网址
+        var curWwwPath = window.document.location.href;
+        //获取主机地址之后的目录
+        var pathName = window.document.location.pathname;
+        var pos = curWwwPath.indexOf(pathName);
+        //获取主机地址
+        var localhostPath = curWwwPath.substring(0, pos);
+        //获取带"/"的项目名
+        var projectName = pathName.substring(0, pathName.substr(1).indexOf('/') + 1);
+        return projectName;
+    }
+    $('.toIndexBtn').click(function () {
+        var type = getCurrentPath();
+        console.log(type);
+    });
 //    get time
     var time = new Date().toLocaleString('chinese', {hour12: false});
     $(".time").text(time);
