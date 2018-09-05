46282c9b2a568e86718d04192336f2106b052d47
Edwin <edwin@Edwin-MacBook-Pro.local>
2018-09-05 17:45:20 +0800

    edwin

--- h5/ba/js/BaRechargeMargin.js
+++ h5/ba/js/BaRechargeMargin.js
@@ -8,11 +8,13 @@ $(function () {
         var base_amount = $('#rechargeMargin').val();
         RechargeManage(token, base_amount, function (response) {
             if(response.errcode == '0'){
+                LayerFun("waitingPro");
                 $('.rechargeFormRow').hide();
                 $('.rechargeAddressRow').show();
                 $('.addressInput').val(response.lgl_address);
             }
         }, function (response) {
+            LayerFun("submissionFailed");
             GetErrorCode(response.errcode);
             return;
         })
