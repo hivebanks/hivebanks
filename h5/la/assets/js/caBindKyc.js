f2aa5f381bbe26228657a7c942667301eade259a
Edwin <edwin@Edwin-MacBook-Pro.local>
2018-09-06 15:56:14 +0800

    edwin

--- h5/la/assets/js/caBindKyc.js
+++ h5/la/assets/js/caBindKyc.js
@@ -6,41 +6,38 @@ $(function () {
     var api_url = 'kyc_ca_list.php', tr = '';
     KycList(api_url, token, function (response) {
         if (response.errcode == '0') {
-            var data = response.rows, bind_info = '', bind_type = '';
+            var data = response.rows, bind_name = '', bind_info = '', bind_type = '';
             if (data == false) {
                 GetDataEmpty('caKyc', '5');
                 return;
             }
             $.each(data, function (i, val) {
                 if (data[i].bind_type == 'file' && data[i].bind_name == 'idPhoto') {
-                    bind_info = "look"; bind_type = "<td><span class='i18n' name='fileBind'></span></td>";
-                    tr += '<tr class="caKycItem">' +
-                        '<td><span class="ca_id">' + data[i].ca_id + '</span></td>' +
-                        '<td style="display: none"><span class="log_id">' + data[i].log_id + '</span></td>' +
-                        bind_type +
-                        // '<td><span>' + data[i].bind_type + '</span></td>' +
-                        '<td><span class="bind_name">' + data[i].bind_name + '</span></td>' +
-                        '<td>' +
-                        '<a href="javascript:;" class="look i18n" name="look">' + bind_info + '</a>' +
-                        '<span class="none idPhotoSrc bind_info">' + data[i].bind_info + '</span>' +
-                        '</td>' +
-                        '<td><span>' + data[i].ctime + '</span></td>' +
-                        '<td><button class="btn btn-success btn-sm passBtn i18n" name="pass"></button></td>' +
-                        '<td><button class="btn btn-danger btn-sm refuseBtn i18n" name="refuse"></button></td>' +
-                        '</tr>';
-                } else {
-                    bind_info = data[i].bind_info;
-                    tr += '<tr class="caKycItem">' +
-                        '<td><span class="ca_id">' + data[i].ca_id + '</span></td>' +
-                        '<td style="display: none"><span class="log_id">' + data[i].log_id + '</span></td>' +
-                        '<td><span>' + data[i].bind_type + '</span></td>' +
-                        '<td><span class="bind_name">' + data[i].bind_name + '</span></td>' +
-                        '<td><a class="bind_info">' + bind_info + '</a></td>' +
-                        '<td><span>' + data[i].ctime + '</span></td>' +
-                        '<td><button class="btn btn-success btn-sm passBtn i18n" name="pass"></button></td>' +
-                        '<td><button class="btn btn-danger btn-sm refuseBtn i18n" name="refuse"></button></td>' +
-                        '</tr>';
+                    bind_type = "<td><span class='i18n' name='fileBind'></span></td>";
+                    bind_name = "<td><span class='bind_name i18n' name='idPhoto'>" + data[i].bind_name + "</span></td>";
+                    bind_info = "<td>" +
+                        "<a href='javascript:;' class='look i18n' name='look'>" + bind_info + "</a>" +
+                        "<span class='none idPhotoSrc bind_info'>" + data[i].bind_info + "</span>" +
+                        "</td>"
+                } else if (data[i].bind_type == 'text' && data[i].bind_name == 'idNum') {
+                    bind_type = "<td><span class='i18n' name='textBind'></span></td>";
+                    bind_name = "<td><span class='bind_name i18n' name='idNum'>" + data[i].bind_name + "</span></td>";
+                    bind_info = "<td><a class='bind_info'>" + data[i].bind_info + "</a></td>"
+                }else if (data[i].bind_type == 'text' && data[i].bind_name == 'name') {
+                    bind_type = "<td><span class='i18n' name='textBind'></span></td>";
+                    bind_name = "<td><span class='bind_name i18n' name='name'>" + data[i].bind_name + "</span></td>";
+                    bind_info = "<td><a class='bind_info'>" + data[i].bind_info + "</a></td>"
                 }
+                tr += "<tr class='caKycItem'>" +
+                    "<td><span class='ca_id'>" + data[i].ca_id + "</span></td>" +
+                    "<td style='display: none'><span class='log_id'>" + data[i].log_id + "</span></td>" +
+                    bind_type +
+                    bind_name +
+                    bind_info +
+                    "<td><span>" + data[i].ctime + "</span></td>" +
+                    "<td><button class='btn btn-success btn-sm passBtn i18n' name='pass'></button></td>" +
+                    "<td><button class='btn btn-danger btn-sm refuseBtn i18n' name='refuse'></button></td>" +
+                    "</tr>"
             });
             $('#caKyc').html(tr);
             execI18n();
