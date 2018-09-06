8046731b0e7c8617e043198f75292b58fa0b3b14
ly <15901839273@163.com>
2018-09-06 11:22:16 +0800

    邮件发送

---
+++ api/ca/get_la_admin_info.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liangyi
+ * Date: 2018/9/6
+ * Time: 上午11:18
+ */
+
+require_once "db/la_admin.php";
+require_once "../inc/common.php";
+
+php_begin();
+$args = array('token');
+chk_empty_args('GET', $args);
+// 用户token
+$token = get_arg_str('GET', 'token', 128);
+//验证token
+$ba_id = check_token($token);
+
+$row = get_la_admin_info();
+
+$rtn_ary = array();
+$rtn_ary['errcode'] = '0';
+$rtn_ary['errmsg'] = '';
+$rtn_ary['row'] = $row;
+
+$rtn_str = json_encode($rtn_ary);
+php_end($rtn_str);
