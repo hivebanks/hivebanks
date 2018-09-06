ac0a5c8ac724b3cbba327f55dbe10cdc53a8c7bf
ly <15901839273@163.com>
2018-09-06 15:38:27 +0800

    邮件发送

--- api/ba/mst_reg_email.php
+++ api/ba/mst_reg_email.php
@@ -1,6 +1,5 @@
 <?php
 require_once '../inc/common.php';
-require_once '../plugin/email/send_email.php';
 require_once 'db/ba_base.php';
 require_once 'db/ba_bind.php';
 require_once "db/com_option_config.php";
@@ -23,7 +22,7 @@ GET参数
 */
 
 php_begin();
-$args = array('email', 'pass_word_hash','pass_word','bit_type');
+$args = array('email', 'pass_word_hash', 'pass_word', 'bit_type');
 chk_empty_args('GET', $args);
 
 // Email地址
@@ -31,29 +30,29 @@ $email = get_arg_str('GET', 'email', 255);
 
 // 密码
 $pass_word = get_arg_str('GET', 'pass_word');
- //密码HASH
+//密码HASH
 $pass_word_hash = get_arg_str('GET', 'pass_word_hash');
 // 验证码
 $cfm_code = get_arg_str('GET', 'cfm_code');
 // 邀请码
 $bit_type = get_arg_str('GET', 'bit_type');
 $is_email = isEmail($email);
-if(!$is_email){
-    exit_error('109','Email format not correct!');
+if (!$is_email) {
+    exit_error('109', 'Email format not correct!');
 }
 //判断密码强度
 $score = Determine_password_strength($pass_word);
 
-if($score <= 3){
-    exit_error('119','密码过于简单请重新设置!');
+if ($score <= 3) {
+    exit_error('119', '密码过于简单请重新设置!');
 }
 $bit_type_row = bit_type_exist_or_not($bit_type);
-if(!$bit_type_row)
-    exit_error("122","当前la不支持此代理类型");
-if(ba_can_reg_or_not()["option_value"] != 1)
-    exit_error("120","当前la未开通注册");
+if (!$bit_type_row)
+    exit_error("122", "当前la不支持此代理类型");
+if (ba_can_reg_or_not()["option_value"] != 1)
+    exit_error("120", "当前la未开通注册");
 // 创建用户ba_id
-$ba_id =get_guid();
+$ba_id = get_guid();
 // 创建用户bind_id
 $bind_id = get_guid();
 // 用户基本信息数组
@@ -64,21 +63,21 @@ $variable = 'email';
 // 当前时间戳
 $timestamp = time();
 // 判断邮箱是否已存在
-$row = get_ba_id_by_variable($variable,$email);
+$row = get_ba_id_by_variable($variable, $email);
 // 获取绑定信息日志表该用户最新的数据
 $teltime = 1;
-$rec = get_ba_log_bind_by_variable($variable , $email);
-if(!$rec){
+$rec = get_ba_log_bind_by_variable($variable, $email);
+if (!$rec) {
     $teltime = 0;
 }
-if($teltime != 0){
-    $teltime = strtotime($rec['ctime']) +15*60 ;
-    if($teltime > $timestamp ){
+if ($teltime != 0) {
+    $teltime = strtotime($rec['ctime']) + 15 * 60;
+    if ($teltime > $timestamp) {
         //判断是否可以进行注册
-        if($rec && $rec['bind_info'] == $email){
-            exit_error('121','待确认，请前往邮箱验证');
+        if ($rec && $rec['bind_info'] == $email) {
+            exit_error('121', '待确认，请前往邮箱验证');
         }
-    }else{
+    } else {
         //把本次的绑定的数据进行无效操作
         $email_used = upd_ba_log_bind_info($ba_id);
     }
@@ -103,8 +102,8 @@ $data_log_bind['bind_type'] = 'text';
 // 绑定登录密码参数整理
 $data_bind_pass = array();
 $data_bind_pass['bind_id'] = get_guid();
-$data_bind_pass['ba_id'] =$ba_id;
-$data_bind_pass['bind_type']  = 'hash';
+$data_bind_pass['ba_id'] = $ba_id;
+$data_bind_pass['bind_type'] = 'hash';
 $data_bind_pass['bind_name'] = 'password_login';
 $data_bind_pass['bind_info'] = $pass_word_hash;
 $data_bind_pass['bind_flag'] = 1;
@@ -113,12 +112,11 @@ $data_base['ba_id'] = $ba_id;
 //加盐加密
 $salt = rand(10000000, 99999999);
 // 邮件地址已经存在
-if($row){
+if ($row) {
     //是否注册验证完成
-    switch ($row['bind_flag'])
-    {
+    switch ($row['bind_flag']) {
         case 1:
-            exit_error('105','Registered users please login directly!');
+            exit_error('105', 'Registered users please login directly!');
             break;
         case 9:
             break;
@@ -126,22 +124,22 @@ if($row){
 }
 
 //判断是否可以验证
-if($rec['limt_time'] > $timestamp){
-    exit_error('116',$rec['limt_time'] - $timestamp);
+if ($rec['limt_time'] > $timestamp) {
+    exit_error('116', $rec['limt_time'] - $timestamp);
 }
-if($rec){
+if ($rec) {
     // 绑定参数设定
     $data_log_bind = $rec;
-    $data_log_bind['count_error'] = $rec['count_error']+1;
-    $data_log_bind['limt_time'] = $timestamp + pow(2,$data_log_bind['count_error']);
+    $data_log_bind['count_error'] = $rec['count_error'] + 1;
+    $data_log_bind['limt_time'] = $timestamp + pow(2, $data_log_bind['count_error']);
     unset($data_log_bind['log_id']);
 }
 $key = Config::TOKEN_KEY;
 $data_base['base_amount'] = 0;
-$data_base['lock_amount'] =0;
+$data_base['lock_amount'] = 0;
 $data_base['ba_level'] = 0;
 $data_base['security_level'] = 2;
-$data_base['ba_account'] = "hivebanks_".$email;
+$data_base['ba_account'] = "hivebanks_" . $email;
 $data_base['utime'] = time();
 $data_base['ctime'] = date("Y-m-d H:i:s");
 
@@ -150,21 +148,43 @@ $data_base['ctime'] = date("Y-m-d H:i:s");
 $url = Config::BA_CONFORM_URL;
 //绑定成功发送验证信息
 //if($bind_email){
-    $timestamp +=15*60;
-    $title = '邮箱验证链接';
-    $des = new Des();
-    $body = $url . "?cfm_hash=";
-    $encryption_code = $ba_id.','.$email.',' . $timestamp .',' .$salt;
-    $body .=urlencode($des -> encrypt($encryption_code, $key));
-    $ret = send_email($name='', $email, $title, $body);
-    if(!$ret){
-        exit_error('124','邮件发送失败请稍后重试！');
+$timestamp += 15 * 60;
+$title = '邮箱验证链接';
+$des = new Des();
+$body = $url . "?cfm_hash=";
+$encryption_code = $ba_id . ',' . $email . ',' . $timestamp . ',' . $salt;
+$body .= urlencode($des->encrypt($encryption_code, $key));
+
+
+$url = "http://agent_service.fnying.com/email/sms_email.php";
+
+$post_data = array("$email" => $email, "$title" => $title,'' => $body);
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+
+$output = curl_exec($ch);
+curl_close($ch);
+
+print_r($output);
+
+die;
+$output_array = json_decode($output, true);
+
+
+    $ret = send_email($name = '', $email, $title, $body);
+    if (!$ret) {
+        exit_error('124', '邮件发送失败请稍后重试！');
     }
     $bind_email = ins_bind_ba_reg_bind_log($data_log_bind);
     $bind_pass = ins_bind_ba_reg_bind_info($data_bind_pass);
     $ret = ins_base_ba_reg_base_info($data_base);
-    if($bind_pass && $bind_email && $ret){
+    if ($bind_pass && $bind_email && $ret) {
         exit_ok('Please verify email as soon as possible!');
-    }else{
-    exit_error('101', 'Create failed! Please try again!');
-}
+    } else {
+        exit_error('101', 'Create failed! Please try again!');
+    }
