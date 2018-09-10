66e10fef21a957e534fae2de3de38a570b0c6cad
Edwin <edwin@Edwin-MacBook-Pro.local>
2018-09-10 11:54:49 +0800

    edwin

--- h5/la/assets/js/userKyc.js
+++ h5/la/assets/js/userKyc.js
@@ -25,10 +25,10 @@ $(function () {
                 }
                 tr += "<tr class='userKycItem'>" +
                     "<td><span class='us_id'>" + data[i].us_id + "</span></td>" +
-                    "<td style='display: none'>" +
-                    "<p class='bind_name'>" + data[i].bind_name + "</p>" +
-                    "<span class='log_id'>" + data[i].log_id + "</span>" +
-                    "</td>" +
+                    "<td style='display: none' class='bind_name'>'+ data[i].bind_name +'</td>" +
+                    // "<p class='bind_name'>" + data[i].bind_name + "</p>" +
+                    // "<span class='log_id'>" + data[i].log_id + "</span>" +
+                    // "</td>" +
                     bind_type +
                     bind_name +
                     bind_info +
@@ -54,7 +54,7 @@ $(function () {
     $(document).on('click', '.passBtn', function () {
         var _this = $(this);
         var us_id = $(this).parents('.userKycItem').find('.us_id').text();
-        var bind_name = $(this).parents('.userKycItem').find('.bind_name');
+        var bind_name = $(this).parents('.userKycItem').find('.bind_name').text();
         var bind_info = $(this).parents('.userKycItem').find('.bind_info').text();
         var log_id = $(this).parents('.userKycItem').find('.log_id').text();
         console.log(bind_name);
