0fed57bd599cd4242043f778d8cb4d97182c6654
Edwin <edwin@Edwin-MacBook-Pro.local>
2018-09-11 18:35:58 +0800

    edwin

--- h5/ca/js/main.js
+++ h5/ca/js/main.js
@@ -230,37 +230,37 @@ function LayerFun(type) {
     layer.msg('<span class="i18n" name="'+ type +'"></span>');
     execI18n();
 }
+
+var opts = {
+    lines: 8, // The number of lines to draw
+    length: 10, // The length of each line
+    width: 2, // The line thickness
+    radius: 10, // The radius of the inner circle
+    scale: 1, // Scales overall size of the spinner
+    corners: 1, // Corner roundness (0..1)
+    color: '#ffffff', // CSS color or array of colors
+    fadeColor: 'transparent', // CSS color or array of colors
+    speed: 1, // Rounds per second
+    rotate: 0, // The rotation offset
+    animation: 'spinner-line-fade-quick', // The CSS animation name for the lines
+    direction: 1, // 1: clockwise, -1: counterclockwise
+    zIndex: 2e9, // The z-index (defaults to 2000000000)
+    className: 'spinner', // The CSS class to assign to the spinner
+    top: '50%', // Top position relative to parent
+    left: '50%', // Left position relative to parent
+    shadow: '0 0 1px transparent', // Box-shadow for the lines
+    position: 'absolute' // Element positioning
+};
+var target = document.getElementById("mySpin");
+var spinner = new Spinner(opts);
+
 //show loading
 function ShowLoading(type) {
-
-    var opts = {
-        lines: 8, // The number of lines to draw
-        length: 10, // The length of each line
-        width: 2, // The line thickness
-        radius: 10, // The radius of the inner circle
-        scale: 1, // Scales overall size of the spinner
-        corners: 1, // Corner roundness (0..1)
-        color: '#ffffff', // CSS color or array of colors
-        fadeColor: 'transparent', // CSS color or array of colors
-        speed: 1, // Rounds per second
-        rotate: 0, // The rotation offset
-        animation: 'spinner-line-fade-quick', // The CSS animation name for the lines
-        direction: 1, // 1: clockwise, -1: counterclockwise
-        zIndex: 2e9, // The z-index (defaults to 2000000000)
-        className: 'spinner', // The CSS class to assign to the spinner
-        top: '50%', // Top position relative to parent
-        left: '50%', // Left position relative to parent
-        shadow: '0 0 1px transparent', // Box-shadow for the lines
-        position: 'absolute' // Element positioning
-    };
-    var target = document.getElementById("mySpin");
-    var spinner = new Spinner(opts);
-    // if(type == "show"){
+    if(type == "show"){
         spinner.spin(target);
-    // }
-    // setTimeout(spinner.spin(), 5000);
-    // if(type == "hide"){
-    //     spinner.spin();
-    // }
+    }
+    if(type == "hide"){
+        spinner.spin();
+    }
 
 }
