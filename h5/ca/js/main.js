eb516484a267f04683adf9aa52c446ec61d95434
Edwin <edwin@Edwin-MacBook-Pro.local>
2018-09-11 18:56:23 +0800

    edwin

--- h5/ca/js/main.js
+++ h5/ca/js/main.js
@@ -235,7 +235,6 @@ function LayerFun(type) {
 var div = $("<div id='mySpin'></div>");
 var script = $("<script src='../../assets/js/spin.min.js'></script>");
 $("body").append(div);
-$("body").append(script);
 var opts = {
     lines: 8, // The number of lines to draw
     length: 10, // The length of each line
