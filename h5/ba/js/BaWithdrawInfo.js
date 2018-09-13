160bf19177aa8ccf1e357a9479657a68314fcea8
Edwin <edwin@Edwin-MacBook-Pro.local>
2018-09-13 20:12:26 +0800

    edwin

--- h5/ba/js/BaWithdrawInfo.js
+++ h5/ba/js/BaWithdrawInfo.js
@@ -7,7 +7,7 @@ $(function () {
             var data = response.rows;
             $('.tx_hash').text(data[0].tx_hash);
             $('.asset_id').text(data[0].asset_id);
-            $('.us_account_id').text(data[0].us_account_id);
+            $('.bit_amount').text(data[0].bit_amount);
             $('.tx_time').text(data[0].tx_time);
             $('.bit_address').text(data[0].bit_address);
             if(data.qa_flag == '0'){
