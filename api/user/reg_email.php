b6a746d7387f53aaca4497c23bac8f596c268424
Bruce <1176932572@qq.com>
2018-09-14 14:11:19 +0800

    Brue

--- api/user/reg_email.php
+++ api/user/reg_email.php
@@ -195,10 +195,14 @@ $url = Config::CONFORM_URL;
 $timestamp += 15 * 60;
 $title = '邮箱验证链接';
 $des = new Des();
-$body ="<h3>亲爱的用户：</h3>当您收到这封邮件时，说明您的注册邮箱是有效的。<br>
-邮箱通过有效验证后，您的账户安全将更有保障。<br>点击此处激活账户 , 如果链接无法点击，请复制并打开以下网址：<br><br>". $url . "?cfm_hash=";
+$body ="<h3>亲爱的用户：</h3>当您收到这封邮件时，说明您的注册邮箱是有效的。
+<br>邮箱通过有效验证后，您的账户安全将更有保障。
+<br>点击此处激活账户 , 如果链接无法点击，请复制并打开以下网址：<br>
+<br>". $url . "?cfm_hash=";
 $encryption_code = $us_id . ',' . $email . ',' . $timestamp . ',' . $salt;
-$body .=  urlencode($des->encrypt($encryption_code, $key))."<br><br><span style='color: red;font-weight: 800'>安全提示</span><br>
+$body .=  urlencode($des->encrypt($encryption_code, $key))."
+<br><br>
+<p style='color: red;font-weight: 800'>安全提示</p><br>
 <span>*不要把您的密码以及本链接告诉任何人！</span><br>
 <span>*开启二次验证（谷歌验证或短信验证)！</span><br>
 <span>如果此活动不是您本人操作，请您尽快联系风赢客服。 </span><br>
