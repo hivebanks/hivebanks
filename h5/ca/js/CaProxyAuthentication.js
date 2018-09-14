1749f0b45aa9c610ae3087372b921612bb53a78c
Edwin <edwin@Edwin-MacBook-Pro.local>
2018-09-14 13:32:27 +0800

    edwin

--- h5/ca/js/CaProxyAuthentication.js
+++ h5/ca/js/CaProxyAuthentication.js
@@ -8,20 +8,23 @@ $(function () {
     var newImgHtml = decodeURIComponent(href);
     $(".newImgBox>img").attr("src", newImgHtml);
 
-    //添加代理类型
+    //Add proxy type
     var ca_channel = newImgHtml.substring(newImgHtml.indexOf('/') + 1, newImgHtml.indexOf('.'));
     $('.addBankBtn').click(function () {
         var card_nm = $('#BankCard').val(), name = $('#Name').val(),
             idNum = $('#IDCard').val(), pass_word_hash = hex_sha1($('#Password').val());
         var $this = $(this), btnText = $(this).text();
         if(DisableClick($this)) return;
+        ShowLoading("show");
         AddAgencyType(token, ca_channel, card_nm, name, idNum, pass_word_hash, function (response) {
             if(response.errcode == '0'){
+                ShowLoading("hide");
                 ActiveClick($this, btnText);
                 LayerFun('setSuccess');
                 window.location.href = 'CaLookAgencyType.html';
             }
         }, function (response) {
+            ShowLoading("hide");
             ActiveClick($this, btnText);
             LayerFun(response.errcode);
             return;
