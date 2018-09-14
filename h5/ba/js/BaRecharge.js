5cae5ae05242bdac173d8484d14c5f154d224185
Edwin <edwin@Edwin-MacBook-Pro.local>
2018-09-14 13:19:12 +0800

    edwin

--- h5/ba/js/BaRecharge.js
+++ h5/ba/js/BaRecharge.js
@@ -102,7 +102,7 @@ $(function () {
                 $('.ruleBtn').text(time + 's');
             } else {
                 clearInterval(timer);
-                $('.ruleBtn').removeClass('disabled', false);
+                $('.ruleBtn').attr('disabled', false);
                 execI18n();
             }
         }, 1000);
