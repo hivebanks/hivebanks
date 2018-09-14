dff1cd77d9feca6fb6da6b13c9a4b144ace97ad9
Edwin <edwin@Edwin-MacBook-Pro.local>
2018-09-14 15:36:19 +0800

    edwin

--- h5/ba/js/main.js
+++ h5/ba/js/main.js
@@ -167,12 +167,15 @@ function GetPhoneCodeFun(bind_type, $this, cfm_code) {
         return;
     }
     setTime($this);
+    ShowLoading("show");
     GetPhoneCode(cellphone, country_code, bind_type, cfm_code, function (response) {
         if (response.errcode == '0') {
+            ShowLoading("hide");
             LayerFun('sendSuc');
             return;
         }
     }, function (response) {
+        ShowLoading("hide");
         LayerFun(response.errcode);
         clearInterval(timer);
         $this.attr("disabled", false);
