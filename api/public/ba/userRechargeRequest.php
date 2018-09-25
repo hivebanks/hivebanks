90666fac176454105fdf6e111440fcce135a06fd
pool <epsilonsunny@gmai.com>
2018-09-25 19:03:16 +0800

    yang

---
+++ api/public/ba/userRechargeRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once '../inc/common.php';
+
+header("cache-control:no-cache,must-revalidate");
+header("Content-Type:application/json;charset=utf-8");
+
+/*
+ * ==========================ba得到用户充值请求=========================
+ * GET参数：
+ * id               id
+ * apikey           ba用户的api key
+ * type            充值类型
+ *  返回：
+ *total           总记录数
+ *rows          记录数组
+ *asset_id            充值资产ID
+ *qa_id               请求ID
+ *us_id               用户ID
+ *bit_address         数字货币充值地址
+ *bit_amount          数组货币金额
+ *base_amount         充值资产金额
+ *tx_hash             交易HASH
+ *tx_time             请求时间戳
+ *说明
+ */
+
+
+php_begin();
+$args = array('token', );
