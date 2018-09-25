55d77ff98cf8eb3dfda1237c3f997f308bc92df9
Edwin <edwin@Edwin-MacBook-Pro.local>
2018-09-25 17:35:27 +0800

    edwin

--- h5/ba/js/main.js
+++ h5/ba/js/main.js
@@ -245,7 +245,7 @@ function LayerFun(type) {
         window.location.href = "login.html";
         return;
     }
-    layer.msg("<span class='i18n' name='" + type + "'></span>", {time:6000});
+    layer.msg("<span class='i18n' name='" + type + "'></span>", {time:10000});
     execI18n();
 }
 
