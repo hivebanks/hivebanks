f75c443e3f9e7c4c470b03d20c6223663024b26f
pool <epsilonsunny@gmai.com>
2018-09-27 18:49:13 +0800

    yang

--- api/ba/mst_reg_email.php
+++ api/ba/mst_reg_email.php
@@ -7,8 +7,7 @@ require_once "db/com_option_config.php";
 require_once '../inc/judge_format.php';
 require_once 'db/ba_log_bind.php';
 require_once "../inc/common_agent_email_service.php";
-header("cache-control:no-cache,must-revalidate");
-header("Content-Type:application/json;charset=utf-8");
+
 
 /*
 ========================== 代理商注册（邮件） ==========================
