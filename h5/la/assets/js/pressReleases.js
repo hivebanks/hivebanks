356d1009acef4289cedae1d183b5ae9030c2ffc4
Edwin <edwin@Edwin-MacBook-Pro.local>
2018-10-09 17:07:32 +0800

    h5

--- h5/la/assets/js/pressReleases.js
+++ h5/la/assets/js/pressReleases.js
@@ -1,7 +1,48 @@
 $(function () {
-    $(".getContentBtn").click(function () {
-        console.log($("#editor").html());
-    })
+    $(function () {
+        //Get token
+        var token = GetCookie('la_token');
+        //get key_code
+        var key_code = "";
+        GetKeyCode(token, function (response) {
+            if (response.errcode == '0') {
+                key_code = response.key_code;
+                console.log(key_code);
+            }
+        }, function (response) {
+            LayerFun(response.errcode);
+        });
+
+        $('.summernote').summernote({
+            height: 200,
+            tabsize: 2,
+            lang: 'zh-CN',
+            focus: true,
+            callbacks: {
+                onImageUpload: function (files, editor, $editable) {
+                    console.log("666");
+                    var formData = new FormData();
+                    formData.append('file', files[0]);
+                    formData.append('key_code', key_code);
+                    $.ajax({
+                        url: 'http://agent_service.fnying.com/upload_file/upload.php',//后台文件上传接口
+                        type: 'POST',
+                        data: formData,
+                        processData: false,
+                        contentType: false,
+                        success: function (data) {
+                            var response = JSON.parse(data), url = response.url;
+                            $('.summernote').summernote('insertImage', url, 'img');
+                        }
+                    });
+                }
+            },
+        });
+
+        $(".getContentBtn").click(function () {
+            console.log($(".summernote").summernote("code"));
+        })
+    });
     // $("#fileInput").change(function () {
     //     var file = this.files[0];
     //     var src = getObjectURL(file);
