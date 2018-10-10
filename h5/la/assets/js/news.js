fd7952676dcce4de1c15c1d38fbb3e81db8bc257
Edwin <edwin@Edwin-MacBook-Pro.local>
2018-10-10 15:39:21 +0800

    h5

--- h5/la/assets/js/news.js
+++ h5/la/assets/js/news.js
@@ -2,37 +2,37 @@ $(function () {
     //get token
     var token = GetCookie("la_token");
 
-        //get news list
-        $(".preloader-wrapper").addClass("active");
-        GetNewsList(token, function (response) {
-            if (response.errcode == "0") {
-                $(".preloader-wrapper").removeClass("active");
-                var data = response.rows, tr = "";
-                if (data == null) {
-                    GetDataEmpty("newsList", "4");
-                    return;
-                }
-                $.each(data, function (i, val) {
-                    tr += "<tr class='newsItem'>" +
-                        "<td><span>" + data[i].title + "</span></td>" +
-                        "<td><span>" + data[i].author + "</span></td>" +
-                        "<td><span>" + data[i].ctime + "</span></td>" +
-                        "<td>" +
-                        "<span class='news_id none'>" + data[i].news_id + "</span>" +
-                        "<button class='btn btn-success modifyNewsBtn i18n' name='modify'>modify</button>" +
-                        "<button class='btn btn-danger margin-left-2 deleteNewsBtn i18n' name='delete'>delete</button>" +
-                        "</td>" +
-                        "</tr>"
-                });
-                $("#newsList").html(tr);
-                execI18n();
-            }
-
-        }, function (response) {
+    //get news list
+    $(".preloader-wrapper").addClass("active");
+    GetNewsList(token, function (response) {
+        if (response.errcode == "0") {
             $(".preloader-wrapper").removeClass("active");
-            GetDataFail("newsList", "4");
-            LayerFun(response.errcode);
-        });
+            var data = response.rows, tr = "";
+            if (data == null) {
+                GetDataEmpty("newsList", "4");
+                return;
+            }
+            $.each(data, function (i, val) {
+                tr += "<tr class='newsItem'>" +
+                    "<td><a href='newsDetail.html?"+ data[i].news_id +"' class='newsTitleClick'>" + data[i].title + "</a></td>" +
+                    "<td><span>" + data[i].author + "</span></td>" +
+                    "<td><span>" + data[i].ctime + "</span></td>" +
+                    "<td>" +
+                    "<span class='news_id none'>" + data[i].news_id + "</span>" +
+                    "<button class='btn btn-success modifyNewsBtn i18n' name='modify'>modify</button>" +
+                    "<button class='btn btn-danger margin-left-2 deleteNewsBtn i18n' name='delete'>delete</button>" +
+                    "</td>" +
+                    "</tr>"
+            });
+            $("#newsList").html(tr);
+            execI18n();
+        }
+
+    }, function (response) {
+        $(".preloader-wrapper").removeClass("active");
+        GetDataFail("newsList", "4");
+        LayerFun(response.errcode);
+    });
 
     //delete news
     $(document).on("click", ".deleteNewsBtn", function () {
