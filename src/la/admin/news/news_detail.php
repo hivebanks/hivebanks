c5226e0e6cae3c90332f5bb6d47196057f6003f5
范助国 <ahino@fanzhuguodeMacBook-Pro.local>
2018-10-10 16:50:46 +0800

    news

--- src/la/admin/news/news_detail.php
+++ src/la/admin/news/news_detail.php
@@ -30,13 +30,13 @@ la_user_check($token);
 $news = news_detail($news_id);
 if($news){
 
-//    $rtn_ary = array();
-//    $rtn_ary['errcode'] = '0';
-//    $rtn_ary['errmsg'] = '';
-//    $rtn_ary['rows'] = $news;
-//    $rtn_str = json_encode($rtn_ary);
-//    php_end($rtn_str);
-    exit_ok($news);
+    $rtn_ary = array();
+    $rtn_ary['errcode'] = '0';
+    $rtn_ary['errmsg'] = '';
+    $rtn_ary['rows'] = $news;
+    $rtn_str = json_encode($rtn_ary);
+    php_end($rtn_str);
+
 }
 
 exit_error('-1','获取新闻详情失败');
