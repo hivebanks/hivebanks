9efeb72a590eae902d923a8cf2cec3707b53bb87
Edwin <edwin@Edwin-MacBook-Pro.local>
2018-10-10 17:22:48 +0800

    h5

--- h5/la/assets/js/newsDetail.js
+++ h5/la/assets/js/newsDetail.js
@@ -7,9 +7,10 @@ $(function () {
     //get news detail
     GetNewsDetail(token, news_id, function (response) {
         if (response.errcode == "0") {
-            console.log(response);
             var data = response.rows;
             $(".newsTitle").text(data[0].title);
+            $(".time").text(data[0].ctime);
+            $(".author").text(data[0].author);
             $(".newsContent").html(data[0].content);
         }
     }, function (response) {
