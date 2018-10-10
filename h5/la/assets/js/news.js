8dbc46517d6df2dcc4f8584019f397aa853fd13c
Edwin <edwin@Edwin-MacBook-Pro.local>
2018-10-10 17:45:51 +0800

    h5

--- h5/la/assets/js/news.js
+++ h5/la/assets/js/news.js
@@ -14,7 +14,7 @@ $(function () {
             }
             $.each(data, function (i, val) {
                 tr += "<tr class='newsItem'>" +
-                    "<td><a href='newsDetail.html?news_id="+ data[i].news_id +"' class='newsTitleClick'>" + data[i].title + "</a></td>" +
+                    "<td><a href='newsDetail.html?news_id=" + data[i].news_id + "' class='newsTitleClick'>" + data[i].title + "</a></td>" +
                     "<td><span>" + data[i].author + "</span></td>" +
                     "<td><span>" + data[i].ctime + "</span></td>" +
                     "<td>" +
@@ -49,6 +49,13 @@ $(function () {
             $(".preloader-wrapper").removeClass("active");
             LayerFun("failedToDelete");
         })
+    });
+
+    //modify news
+    $(document).on("click", ".modifyNewsBtn", function () {
+        var this_news_id = $(this).siblings(".news_id").text();
+        window.location.href = "pressReleases.html?this_news_id=" + this_news_id;
     })
+
 
 });
