73bd6bf6ab0624ce0177b5c71c7a2b8251f788ac
Edwin <edwin@Edwin-MacBook-Pro.local>
2018-10-11 11:34:06 +0800

    h5

--- h5/news/js/news.js
+++ h5/news/js/news.js
@@ -34,20 +34,29 @@ $(function () {
         }
     });
 
-    var smokyBG = $('#smoky-bg').waterpipe({
-        gradientStart: '#51ff00',
-        gradientEnd: '#001eff',
-        smokeOpacity: 0.1,
-        smokeSize: 100,
-        numCircles: 1,
-        maxMaxRad: 'auto',
-        minMaxRad: 'auto',
-        minRadFactor: 0,
-        iterations: 8,
-        drawsPerFrame: 10,
-        lineWidth: 2,
-        speed: 10,
-        bgColorInner: "#111",
-        bgColorOuter: "#000"
-    });
+    //get news list
+    Get_News_List(function (response) {
+        if(response.errcode == "0"){
+            console.log(response.rows);
+        }
+    }, function (response) {
+        LayerFun(response.errcode);
+    })
+
+    // var smokyBG = $('#smoky-bg').waterpipe({
+    //     gradientStart: '#51ff00',
+    //     gradientEnd: '#001eff',
+    //     smokeOpacity: 0.1,
+    //     smokeSize: 100,
+    //     numCircles: 1,
+    //     maxMaxRad: 'auto',
+    //     minMaxRad: 'auto',
+    //     minRadFactor: 0,
+    //     iterations: 8,
+    //     drawsPerFrame: 10,
+    //     lineWidth: 2,
+    //     speed: 10,
+    //     bgColorInner: "#111",
+    //     bgColorOuter: "#000"
+    // });
 });
