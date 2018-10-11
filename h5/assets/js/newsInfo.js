92f4f7c66b29a5792c932bf3c1e2c5eb39c56ee9
Edwin <edwin@Edwin-MacBook-Pro.local>
2018-10-11 12:48:48 +0800

    h5

--- h5/assets/js/newsInfo.js
+++ h5/assets/js/newsInfo.js
@@ -7,7 +7,7 @@ $(function () {
     var news_id = GetQueryString("news_id");
 
     //get news info
-    function GetNewsInfoFun(news_id){
+    function GetNewsInfoFun(news_id) {
         GetNewsInfo(news_id, function (response) {
             if (response.errcode == "0") {
                 var data = response.rows;
@@ -20,20 +20,26 @@ $(function () {
             return;
         });
     }
+
     GetNewsInfoFun(news_id);
 
     $(document).on("click", ".leftNewsTitle", function () {
         var news_id = $(this).attr("name");
-       GetNewsInfoFun(news_id);
+        GetNewsInfoFun(news_id);
     });
 
     //get news list
     Get_News_List(function (response) {
         if (response.errcode == "0") {
             var data = response.rows;
-            var li = "";
+            var li = "", li_first = "", li_other = "";
             $.each(data, function (i, val) {
-                li += "<li class='leftNewsTitle' title='"+ data[i].title +"' name='"+ data[i].news_id +"'>"+ data[i].title +"</li>"
+                if (data[0]) {
+                    li_first = "<li class='leftNewsTitle' title='" + data[0].title + "' name='" + data[0].news_id + "'>" + data[0].title + "</li>";
+                    li_other += "<li class='leftNewsTitle' title='" + data[i + 1].title + "' name='" + data[i + 1].news_id + "'>" + data[i + 1].title + "</li>"
+                }
+                li = li_first + li_other
+
             });
             $(".newsInfo_nav").html(li);
 
