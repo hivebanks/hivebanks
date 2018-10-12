c269dfcaec705318f2ca3a49e942cbf5b3749e21
Edwin <edwin@Edwin-MacBook-Pro.local>
2018-10-12 15:56:24 +0800

    h5

--- h5/user/js/account.js
+++ h5/user/js/account.js
@@ -128,8 +128,8 @@ $(function () {
 
         $('#qrcode').qrcode({
             text: url,
-            // width: 570,
-            // height: 870
+            width: 570,
+            height: 870
         });
         // console.log($("#qrcode").html());
         //canvas invite img
